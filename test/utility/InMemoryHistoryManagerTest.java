85403e273d6fe562359b243ae5ed7954264cc730
Kost7592 <k.mihailow2018@yandex.ru>
2024-05-14 00:44:30 +0400

    Выполнение ТЗ 5

--- test/utility/InMemoryHistoryManagerTest.java
+++ test/utility/InMemoryHistoryManagerTest.java
@@ -1,7 +1,55 @@
 package utility;
 
-import static org.junit.jupiter.api.Assertions.*;
+import modules.Epic;
+import modules.Subtask;
+import modules.Task;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class InMemoryHistoryManagerTest {
+    private InMemoryTaskManager manager;
+    private InMemoryHistoryManager historyManager;
+    @BeforeEach
+    public void initialization() {
+        manager = new InMemoryTaskManager();
+        historyManager = new InMemoryHistoryManager();
+    }
+
+    @Test
+    public void checkingThatEpicDoesNotChangeInHistory() {
+        Epic epic = manager.createEpic(new Epic("a", ""));
+        manager.getEpicById(epic.getId());
+        epic.setName("b");
+        manager.updateEpic(epic);
+
+        Assertions.assertNotEquals(historyManager.getHistory().getFirst().getName(),
+                manager.getEpicById(epic.getId()).getName());
+    }
+
+    @Test
+    public void checkingThatSubtaskDoesNotChangeInHistory() {
+        Epic epic = manager.createEpic(new Epic("", ""));
+        Subtask subtask = manager.createSubtask(new Subtask("a", "", epic.getId()));
+        manager.getSubtaskById(subtask.getId());
+        subtask.setName("b");
+        manager.updateSubtask(subtask);
+
+        Assertions.assertNotEquals(
+                historyManager.getHistory().getFirst().getName(),
+                manager.getSubtaskById(subtask.getId()).getName());
+    }
+
+    @Test
+    public void checkingThatTaskDoesNotChangeInHistory() {
+        Task task = manager.createTask(new Task("a", ""));
+        manager.getTaskById(task.getId());
+        task.setName("b");
+        manager.updateTask(task);
+
+        Assertions.assertNotEquals(
+                historyManager.getHistory().getFirst().getName(),
+                manager.getTaskById(task.getId()).getName());
+    }
 
 }
--- test/utility/InMemoryTaskManagerTest.java
+++ test/utility/InMemoryTaskManagerTest.java
@@ -2,15 +2,17 @@ package utility;
 
 import modules.Epic;
 import modules.Subtask;
+import modules.Task;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InMemoryTaskManagerTest {
-    private TaskManager manager;
+    private InMemoryTaskManager manager;
+
     @BeforeEach
     public void initialization() {
-        manager = Managers.getDefault();
+        manager = new InMemoryTaskManager();
     }
 
     @Test
@@ -20,11 +22,69 @@ class InMemoryTaskManagerTest {
     }
 
     @Test
-    public void checkThatManagerCanCreateAndGiveEpicById() {
+    public void checkThatManagerCanCreateEpicAndGiveItById() {
         Epic epic = manager.createEpic(new Epic("",""));
-        Assertions.assertNotNull(manager.getEpicById(epic.getId()));
+        Assertions.assertNotNull(epic.getId());
     }
 
     @Test
-    public void
+    public void checkThatManagerCanCreateSubtaskAndGiveItById() {
+        Epic epic = manager.createEpic(new Epic("",""));
+        Subtask subtask = manager.createSubtask(new Subtask("", "", epic.getId()));
+        Assertions.assertNotNull(manager.getSubtaskById(subtask.getId()));
+    }
+
+    @Test
+    public void checkThatManagerCanCreateTaskAndGiveItById() {
+        Task task = manager.createTask(new Task("", ""));
+        Assertions.assertNotNull(manager.getTaskById(task.getId()));
+    }
+
+    @Test
+    public void epicShouldNotChangeWhenValueChanged() {
+        Epic epic = manager.createEpic(new Epic("", ""));
+        epic.setName("Другое имя");
+        Assertions.assertNotEquals(epic.getName(), manager.getEpicById(epic.getId()).getName());
+    }
+
+    @Test
+    public void epicShouldNotChangeWhenSourceChanged() {
+        Epic source = new Epic("", "");
+        Epic epic = manager.createEpic(source);
+        source.setName("Другое имя");
+        Assertions.assertNotEquals(source.getName(), manager.getEpicById(epic.getId()).getName());
+    }
+
+    @Test
+    public void subtaskShouldNotChangeWhenValueChanged() {
+        Epic epic = manager.createEpic(new Epic("", ""));
+        Subtask subtask = manager.createSubtask(new Subtask("a", "", epic.getId()));
+        subtask.setName("b");
+        Assertions.assertNotEquals(subtask.getName(), manager.getSubtaskById(subtask.getId()).getName());
+    }
+
+    @Test
+    public void subtaskShouldNotChangeWhenSourceChanged() {
+        Epic epic = manager.createEpic(new Epic("", ""));
+        Subtask source = manager.createSubtask(new Subtask("", "", epic.getId()));
+        Subtask subtask = manager.createSubtask(new Subtask("" ,"", epic.getId()));
+        source.setName("Другое имя");
+        Assertions.assertNotEquals(source.getName(), manager.getSubtaskById(subtask.getId()).getName());
+    }
+
+    @Test
+    public void taskShouldNotChangeWhenValueChanged() {
+        Task task = manager.createTask(new Task("", ""));
+        task.setName("Другое имя");
+        Assertions.assertNotEquals(task.getName(), manager.getEpicById(task.getId()).getName());
+    }
+
+    @Test
+    public void taskShouldNotChangeWhenSourceChanged() {
+        Task source = new Task("", "");
+        Task task = manager.createTask(new Task("", ""));
+        source.setName("Другое имя");
+        Assertions.assertNotEquals(source.getName(), manager.getTaskById(task.getId()).getName());
+    }
+
 }
